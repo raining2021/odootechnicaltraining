d67c07bffaa40a60a820abead33178da2c409a51
Master <master@raining.ai>
2021-11-01 21:54:09 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name' : 'Odoo Academy',
+    'summary' : """Academy app to manage Training""",
+    'description' : """
+    
+        Academy Module to manage Training:
+        - Coursers
+        - Sessions
+    
+    """,
+    'author' : 'Odoo',
+    'website' : '',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [],
+    'demo' : [],
+    
+}
