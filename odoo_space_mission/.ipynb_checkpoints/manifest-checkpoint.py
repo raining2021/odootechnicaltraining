1648f5074b8bcca34c93fd75d4f25d2ddf10ae6d
raining2021 <master@raining.ai>
2021-11-03 22:47:21 +0000

    [ADD] Space Module

---
+++ odoo_space_mission/.ipynb_checkpoints/manifest-checkpoint.py
@@ -0,0 +1,25 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name' : 'Odoo Space Mission',
+    'summary' : """Space mission to orginize the logistics""",
+    'description' : """
+    
+        Academy Module to manage organize Logistic:
+        - Spaceship
+        - Space Crew
+    
+    """,
+    'author' : 'Odoo',
+    'website' : '',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [
+
+    ],
+    'demo' : [
+        
+    ],
+    
+}
